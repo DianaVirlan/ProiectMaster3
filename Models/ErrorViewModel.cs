f543ad2f0af7d8c591b242bc0e8198881766800e
DianaV <dianavirlan25@yahoo.com>
2021-01-11 23:25:43 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProiectMaster3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
